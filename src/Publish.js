b26e06499c7b33c61b3a3b7316d08a8b78330856
Sean La Tourette <latourettes@live.com>
2021-11-10 20:06:07 -0800

    tab bars now scroll together

--- src/Publish.js
+++ src/Publish.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { tabify } from "./services/utils";
+import { ScrollSync, ScrollSyncPane } from "react-scroll-sync";
 
 function Publish() {
   const tabObj = JSON.parse(window.localStorage.getItem("tabObj"));
@@ -12,134 +13,181 @@ function Publish() {
   const e2String = tabify(tabObj.e2);
 
   return (
-    <div
-      style={{
-        padding: "2rem",
-        margin: "2rem",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <div style={{ textAlign: "left" }}>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderRadius: "3px 3px 0 0",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          e |{e2String.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          b |{bString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          g |{gString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          d |{dString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          a |{aString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderRadius: "0 0 3px 3px",
-            color: "#FBB03B",
-          }}
-        >
-          e |{eString.join("")}
-        </div>
-
-        <footer
-          style={{
-            position: "fixed",
-            bottom: "0",
-            left: "0",
-            right: "0",
-            width: "100%",
-          }}
-        >
-          <Link to="/">
-            <button
+    <ScrollSync>
+      <div
+        style={{
+          padding: "2rem",
+          margin: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ textAlign: "left" }}>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
               style={{
-                width: "50%",
-                height: "3rem",
-                backgroundColor: "#171717",
-                color: "#DFDFDF",
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
                 borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
               }}
             >
-              Record Again
-            </button>
-          </Link>
-          <Link to="/edit">
-            <button
+              e |{e2String.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
               style={{
-                width: "50%",
-                height: "3rem",
-                backgroundColor: "#171717",
-                color: "#DFDFDF",
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
                 borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
               }}
             >
-              Edit
-            </button>
-          </Link>
-        </footer>
+              b |{bString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              g |{gString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              d |{dString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              a |{aString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                color: "#FBB03B",
+              }}
+            >
+              e |{eString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <footer
+            style={{
+              position: "fixed",
+              bottom: "0",
+              left: "0",
+              right: "0",
+              width: "100%",
+            }}
+          >
+            <Link to="/">
+              <button
+                style={{
+                  width: "50%",
+                  height: "3rem",
+                  backgroundColor: "#171717",
+                  color: "#DFDFDF",
+                  borderColor: "#FBB03B",
+                }}
+              >
+                Record Again
+              </button>
+            </Link>
+            <Link to="/edit">
+              <button
+                style={{
+                  width: "50%",
+                  height: "3rem",
+                  backgroundColor: "#171717",
+                  color: "#DFDFDF",
+                  borderColor: "#FBB03B",
+                }}
+              >
+                Edit
+              </button>
+            </Link>
+          </footer>
+        </div>
       </div>
-    </div>
+    </ScrollSync>
   );
 }
 
--- src/Tab.js
+++ src/Tab.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { tabify, generateTabObj } from "./services/utils";
+import { ScrollSync, ScrollSyncPane } from "react-scroll-sync";
 
 function Tab({ noteArray, position, setPosition }) {
   const handleButtonPositionUp = () => {
@@ -29,96 +30,181 @@ function Tab({ noteArray, position, setPosition }) {
   const e2String = tabify(tabObj.e2);
 
   return (
-    <div
-      style={{
-        padding: "2rem",
-        margin: "2rem",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <div style={{ textAlign: "left" }}>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderRadius: "3px 3px 0 0",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          e |{e2String.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          b |{bString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          g |{gString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          d |{dString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderBottomStyle: "none",
-            color: "#FBB03B",
-          }}
-        >
-          a |{aString.join("")}
-        </div>
-        <div
-          style={{
-            textOverflow: "clip",
-            borderStyle: "solid",
-            listStyleType: "none",
-            marginRight: "2.5rem",
-            borderColor: "#FBB03B",
-            borderRadius: "0 0 3px 3px",
-            color: "#FBB03B",
-          }}
-        >
-          e |{eString.join("")}
-        </div>
+    <ScrollSync>
+      <div
+        style={{
+          padding: "2rem",
+          margin: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ textAlign: "left" }}>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              e |{e2String.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              b |{bString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              g |{gString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              d |{dString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                borderBottomStyle: "none",
+                color: "#FBB03B",
+              }}
+            >
+              a |{aString.join("")}
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div
+              className="tab-bar"
+              style={{
+                height: "1.5rem",
+                width: "95%",
+                overflowX: "auto",
+                overflowY: "hidden",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                borderStyle: "solid",
+                listStyleType: "none",
+                marginRight: "2.5rem",
+                borderColor: "#FBB03B",
+                color: "#FBB03B",
+              }}
+            >
+              e |{eString.join("")}
+            </div>
+          </ScrollSyncPane>
 
+          <footer
+            style={{
+              position: "fixed",
+              bottom: "0",
+              left: "0",
+              right: "0",
+              width: "100%",
+            }}
+          >
+            <Link to="/">
+              <button
+                style={{
+                  width: "50%",
+                  height: "3rem",
+                  backgroundColor: "#171717",
+                  color: "#DFDFDF",
+                }}
+              >
+                Record Again
+              </button>
+            </Link>
+            <Link to="/edit">
+              <button
+                style={{
+                  width: "50%",
+                  height: "3rem",
+                  backgroundColor: "#171717",
+                  color: "#DFDFDF",
+                }}
+              >
+                Edit
+              </button>
+            </Link>
+          </footer>
+        </div>
+        <div>
+          <p style={{ color: "#FBB03B" }}>Position: {position + 1}</p>
+        </div>
         <footer
           style={{
             position: "fixed",
@@ -131,22 +217,48 @@ function Tab({ noteArray, position, setPosition }) {
           <Link to="/">
             <button
               style={{
-                width: "50%",
+                width: "25%",
                 height: "3rem",
                 backgroundColor: "#171717",
                 color: "#DFDFDF",
+                borderColor: "#FBB03B",
               }}
             >
               Record Again
             </button>
           </Link>
+          <button
+            style={{
+              width: "25%",
+              height: "3rem",
+              backgroundColor: "#171717",
+              color: "#DFDFDF",
+              borderColor: "#FBB03B",
+            }}
+            onClick={handleButtonPositionUp}
+          >
+            Position +
+          </button>
+          <button
+            style={{
+              width: "25%",
+              height: "3rem",
+              backgroundColor: "#171717",
+              color: "#DFDFDF",
+              borderColor: "#FBB03B",
+            }}
+            onClick={handleButtonPositionDown}
+          >
+            Position -
+          </button>
           <Link to="/edit">
             <button
               style={{
-                width: "50%",
+                width: "25%",
                 height: "3rem",
                 backgroundColor: "#171717",
                 color: "#DFDFDF",
+                borderColor: "#FBB03B",
               }}
             >
               Edit
@@ -154,70 +266,7 @@ function Tab({ noteArray, position, setPosition }) {
           </Link>
         </footer>
       </div>
-      <div>
-        <p style={{ color: "#FBB03B" }}>Position: {position + 1}</p>
-      </div>
-      <footer
-        style={{
-          position: "fixed",
-          bottom: "0",
-          left: "0",
-          right: "0",
-          width: "100%",
-        }}
-      >
-        <Link to="/">
-          <button
-            style={{
-              width: "25%",
-              height: "3rem",
-              backgroundColor: "#171717",
-              color: "#DFDFDF",
-              borderColor: "#FBB03B",
-            }}
-          >
-            Record Again
-          </button>
-        </Link>
-        <button
-          style={{
-            width: "25%",
-            height: "3rem",
-            backgroundColor: "#171717",
-            color: "#DFDFDF",
-            borderColor: "#FBB03B",
-          }}
-          onClick={handleButtonPositionUp}
-        >
-          Position +
-        </button>
-        <button
-          style={{
-            width: "25%",
-            height: "3rem",
-            backgroundColor: "#171717",
-            color: "#DFDFDF",
-            borderColor: "#FBB03B",
-          }}
-          onClick={handleButtonPositionDown}
-        >
-          Position -
-        </button>
-        <Link to="/edit">
-          <button
-            style={{
-              width: "25%",
-              height: "3rem",
-              backgroundColor: "#171717",
-              color: "#DFDFDF",
-              borderColor: "#FBB03B",
-            }}
-          >
-            Edit
-          </button>
-        </Link>
-      </footer>
-    </div>
+    </ScrollSync>
   );
 }
 
